541a371f133a6c9eb5a084704cbbc9ae11899464
Abdul-Ismail <abduul.ismail@gmail.com>
2017-08-31 15:20:50 +0100

    User can now pick a profile picture when signing up

--- InstgramClone/SignUpViewController.swift
+++ InstgramClone/SignUpViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import FirebaseAuth
 import FirebaseDatabase
+import FirebaseStorage
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var profileImage: UIImageView!
+    
+    var selectedImage: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,8 +52,21 @@ class SignUpViewController: UIViewController {
         profileImage.layer.cornerRadius = 117/2
         profileImage.clipsToBounds = true // this view should be confined to its underlying CA layer when its rendered
         
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.handleSelectProfileImageView))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
+        
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    //to click the picture for profile picture when user clicks to change profile picture
+    func handleSelectProfileImageView(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+        print("DONE")
     }
 
     @IBAction func dismiss_onClick(_ sender: Any) {
@@ -63,15 +79,44 @@ class SignUpViewController: UIViewController {
                 print(error!.localizedDescription)
                 return
             }
-             let ref = Database.database().reference() //returns reference of where the database is 
+            
+            let uid = user?.uid
+            let storageRef = Storage.storage().reference(forURL: "gs://instagramclone879.appspot.com").child("profile_image").child(uid!)
+            if let profileImage = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImage, 0.1){
+                storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+            if error != nil {
+                return
+            }
+            
+            let profileImageUrl = metadata?.downloadURL()?.absoluteString
+         
+            
+            
+            let ref = Database.database().reference() //returns reference of where the database is
             let userReference = ref.child("users")
-            // print(userReference.description())
             let uid = user?.uid
             let newUserReference = userReference.child(uid!)
-            newUserReference.setValue(["username": self.usernameTextField.text!, "email": self.emailTextField.text!])
-           
+            newUserReference.setValue(["username": self.usernameTextField.text!, "email": self.emailTextField.text!, "profileImageUrl": profileImageUrl])
+            })
+            }
+            
         })
+        
+        
     }
-    
+}
 
+
+
+
+//store the image picked when user is signing up, storing it here before it us pushed onto the database
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("finished pickig media")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
